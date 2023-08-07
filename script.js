5fd7c02fe474e303435a68a86cd947af4bb349d9
Sam <samnewell91@gmail.com>
2023-08-07 20:11:04 +0100

    Able to add items and price to list
    Used Jquery to make button functional

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
- 
+// Add item & cost
+$('#button').click(function() {
+    var item = $('#item').val();
+    var cost = $('#cost').val();
+    $('.item-list').append('<li>' + item + '</li>');
+    $('.receipt-list').append('<li>' + cost + '<li>');
+    $('input[name=item]').val('');
+    $('input[name=cost]').val('');
+  });
+
+// Clear List
+
+$('#trash').click(function (){
+    $('.item-list li').remove();
+    $('.receipt-list li').remove();
+});
